1a2c5acbdb52a82f2c2ff1bd39283dd856331bdf
Phanindra Dokku <phanindradokku@gmail.com>
2024-03-29 02:47:02 -0500

    Delete shopping directory

--- shopping/api/AuthenticationApiService.js
+++
@@ -1,9 +0,0 @@
-import { apiClient } from "./ApiClient"
-export const executeBasicAuthenticationService =
-    (token) => apiClient.get(`/basicAuth`,{
-        headers:{ 
-            Authorization: token 
-        }})
-
-export const executeJwtAuthenticationService =
-    (username,password) => apiClient.post(`/authenticate`,{username,password})
